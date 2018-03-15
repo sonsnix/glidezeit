7d81d274e9dad5fd46a53afd19f0d30966944141
Markus Sons <markus.sons+git@gmail.com>
2018-03-15 20:56:18 +0100

    trying to estimate thermals

--- src/components/GlideZeit.vue
+++ src/components/GlideZeit.vue
@@ -1,7 +1,6 @@
 <template>
     <v-app>
         <v-toolbar color="indigo" dark fixed app>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
             <v-toolbar-title>GlideZeit</v-toolbar-title>
         </v-toolbar>
         
@@ -21,15 +20,19 @@
                                 <h3 class="headline mb-0">Settings</h3>
                             </v-card-title>
                             
+                            <v-subheader>Forecast Location & Model</v-subheader>
                             <v-card-text>
-                                <div>
-                                    <v-text-field  label="fcLocation" v-model="fcLocation"/>
-                                    <v-select label="Model" v-model="fcModel" :items="fcModels"/>
-                                </div>
+                                <v-select label="Location" v-model="fcLocation" :items="fcLocations"/>
+                                <v-select label="Model" v-model="fcModel" :items="fcModels"/>
                             </v-card-text>
 
                             <v-btn @click="requestForecastData">Get FC</v-btn>
 
+                            <v-subheader>Time</v-subheader>
+                            <v-card-text>
+                                <v-slider :min=0 :max=hours.length-1 v-model="skewTime"></v-slider>
+                                FC time: {{ skewTimeDisplay }}
+                            </v-card-text>
                         </v-card>
                     </v-flex>
                 </v-layout>
@@ -49,20 +52,24 @@ import Vue from "vue";
 import * as Plotly from "plotly.js/lib/core";
 
 export default Vue.extend({
-    data() : {constants: any, layout: Partial<Plotly.Layout>, plotData: Plotly.Data[], fcData: any, fcLocation: string, fcModel: string, fcModels: string[], skew: number, apiURL: string} {
+    data() : {apiURL: string, constants: any, firstPlot: boolean, layout: Partial<Plotly.Layout>, plotData: Plotly.Data[], fcData: { temperatures: number[][], dewPoints: number[][] },
+              fcLocation: string, fcLocations: string[], fcLocationCoordinates: any, fcModel: string, fcModels: string[], hours: number[], skew: number, skewTime: number } {
         return {
             constants: {
                 pressureLevels: [1000.0, 950.0, 925.0, 900.0, 850.0, 800.0, 700.0, 600.0, 500.0, 400.0, 300.0, 200.0, 150.0]
             },
             plotData: [],
-            fcData: {temperatures: [], dewPoints: []},
-            fcLocation: "47.412/12.245",
+            fcData: { temperatures: [], dewPoints: [] },
+            fcLocation: "Sammenheim", //47.412/12.245
+            fcLocations: ["Choralpe", "Phoenix", "Sammenheim", "Tauchersreuth"],
+            fcLocationCoordinates: {"Choralpe": "47.419/12.244", "Phoenix": "33.500/-112.142", "Sammenheim": "49.049/10.743", Tauchersreuth: "49.547/11.210"},
             fcModel: "ecmwf",
-            fcModels: ["ecmwf", "gfs"],
+            fcModels: ["ecmwf", "gfs", "iconEu"],
+            hours: [0],
             layout: {
                 xaxis: {
                     type: "linear",
-                    range: [-50, 40],
+                    range: [-40, 40],
                     showline: true,
                     zeroline: false,
                 },
@@ -73,74 +80,199 @@ export default Vue.extend({
                 },
             },
             skew: 35.0,
+            skewTime: 0,
             apiURL: "https://node.windy.com/forecast/meteogram",
+            firstPlot: true,
         }  
     },
+
+    watch: {
+        skewTime: function() {
+            this.plotForecastData();
+        }
+    },
+
+    computed: {
+        skewTimeDisplay: function() : string {
+            if(this.skewTime < this.hours.length) {
+                return new Date(this.hours[this.skewTime]).toUTCString();
+            }
+            else {
+                return "";
+            }
+        }
+    },
+    
     methods: {
         requestForecastData: function() {
-            fetch(this.apiURL + "/" + this.fcModel + "/" + this.fcLocation).then( (response) => {
+            fetch(this.apiURL + "/" + this.fcModel + "/" + this.fcLocationCoordinates[this.fcLocation]).then( (response) => {
                 return response.json();
             }).then( (responseData) => {
                 this.processForecastData(responseData);
             });
         },
 
+        estimateThermals: function() {
+            const pls = this.constants.pressureLevels;
+            const temps = this.fcData.temperatures;
+            let shapes: any[] = [];
+
+            for (let i = 0; i < pls.length-1; i++) {
+                let temp = temps[i][this.skewTime];
+                let tempAtAltitude = temps[i+1][this.skewTime];
+
+                let liftedTemp = (temp+273.15)*Math.pow(pls[i+1]/pls[i], (1.4-1.0)/1.4)-273.15;
+
+                let a_lift = (temp+273.15)*(1.4-1.0)/1.4/pls[i+1]*Math.pow(pls[i+1]/pls[i], (1.4-1.0)/1.4)
+                let b_lift = temp - a_lift*pls[i];
+               
+                if(liftedTemp > tempAtAltitude) {
+                    let shape = {
+                        type: 'rect',
+                        xref: 'paper',
+                        yref: 'y',
+                        x0: '0',
+                        y0: pls[i],
+                        x1: '1',
+                        y1: pls[i+1],
+                        fillcolor: 'yellow',
+                        opacity: 0.2,
+                        line: {
+                            width: 0
+                        }
+                    }
+
+                    shapes.push(shape);
+                }
+            };
+
+            Plotly.relayout("skew-chart", {shapes: shapes});
+        },
+
         processForecastData: function(r : any) {
 
             const pls : number[] = this.constants.pressureLevels;
 
-            let traces : Plotly.Data[] = [];
-            let temperatures: number[] = [];
-            let dewPoints: number[] = [];
+            // reset temperature and dewpoint arrays
+            this.fcData.temperatures = [];
+            this.fcData.dewPoints = [];
+
+            this.hours = r.data["hours"];
 
             // save forecast data
             pls.forEach((pl: number, i: number) => {
-                this.fcData.temperatures[i] = r.data["temp-" + pl.toString() + "h"][0] - 273.15;
-                this.fcData.dewPoints[i] = r.data["dewpoint-" + pl.toString() + "h"][0] - 273.15;
+
+                this.fcData.temperatures[i] = [];
+                this.fcData.dewPoints[i] = [];
+
+                r.data["temp-" + pl.toString() + "h"].forEach((temp: number, j: number) => {
+                    this.fcData.temperatures[i][j] = temp - 273.15;
+                });
+
+                r.data["dewpoint-" + pl.toString() + "h"].forEach((dewPoint: number, j: number) => {
+                    this.fcData.dewPoints[i][j] = dewPoint - 273.15;
+                });
             });
+
+            this.plotForecastData();
+        },
+
+        plotForecastData: function() {
+            const pls : number[] = this.constants.pressureLevels;
+            
+            let traces : Plotly.Data[] = [];
+            let x_temperatures: number[] = [];
+            let x_dewPoints: number[] = [];
 
             // calculate skew T position
             pls.forEach((pl, i) => {
-                temperatures.push(this.fcData.temperatures[i] + this.skew * Math.log(pls[0] / pl));
+                x_temperatures.push(this.fcData.temperatures[i][this.skewTime] + this.skew * Math.log(pls[0] / pl));
             });
 
             pls.forEach((pl, i) => {
-                dewPoints.push(this.fcData.dewPoints[i] + this.skew * Math.log(pls[0] / pl));
+                x_dewPoints.push(this.fcData.dewPoints[i][this.skewTime] + this.skew * Math.log(pls[0] / pl));
             });
 
-            traces.push({ x: temperatures, y: pls, mode: "lines", line: { color: "red" }, name: "Temperature"});
-            traces.push({ x: dewPoints, y: pls, mode: "lines", line: { color: "blue" }, name: "Dew Point"});
+            traces.push({ x: x_temperatures, y: pls, mode: "lines", line: { color: "red" }, name: "Temperature"});
+            traces.push({ x: x_dewPoints, y: pls, mode: "lines", line: { color: "blue" }, name: "Dew Point"});
 
             // delete temp and dew point traces, and re-add
-            //Plotly.deleteTraces("skew-chart", [-2, -1]);
+            if(this.firstPlot == false) {
+                Plotly.deleteTraces("skew-chart", [-2, -1]);
+            }
+
+            this.firstPlot = false;
+
             Plotly.addTraces("skew-chart", traces);
             Plotly.Plots.resize("skew-chart");
+
+            this.estimateThermals();
         },
 
         setupSkewT: function() {
 
-            const pls : number[] = this.constants.pressureLevels;
-            const isotherms = [-100, -80, -60, -40, -20, 0, 20, 40, 60];
+            const pls: number[] = Array.from(Array(51), (_, x) => 1000-x*(1000-100)/50);
+            const isotherms: number[] = Array.from(Array(20), (_, x) => x*10-100);
 
-            let isothermTraces: any[] = [];
-            
+            let traces: any[] = [];
+           
+            // isotherms
             isotherms.forEach((isotherm: number) => {
 
-                let x : number[] = [];
-
-                pls.forEach((pl: number, i: number) => {
-                    x.push(isotherm + this.skew * Math.log(pls[0] / pl));
+                let x: number[] = pls.map( (pl:number) => {
+                    return isotherm + this.skew * Math.log(pls[0] / pl);
                 });
                 
-                isothermTraces.push({x: x, y: pls, mode: "lines", line: { color: "green"}, showlegend: false});
+                traces.push({x: x, y: pls, mode: "lines", line: { color: "green", width: 0.8 }, showlegend: false});
             });
 
-            Plotly.plot("skew-chart", isothermTraces, this.layout, {displayModeBar: false});
+            // dry adiabates
+            isotherms.forEach((isotherm: number) => {
+
+                let x: number[] = pls.map( (pl:number) => {
+                    return (isotherm+273.15)*Math.pow(pl/1000.0, (1.4-1.0)/1.4)-273.15 + this.skew * Math.log(pls[0] / pl);
+                });
+                
+                traces.push({x: x, y: pls, mode: "lines", line: { color: "orange", width: 0.8 }, showlegend: false});
+            });
+
+            // moist adiabates
+            isotherms.forEach((isotherm: number) => {
+                let x: number[] = [];
+                
+                x[0] = isotherm;
+
+                for(let i = 1; i < pls.length; i++) {
+                    let e_s = 0.6113 * Math.exp(17.2694*x[i-1]/(x[i-1]+273.15-35.86));
+                    // let e_s = 0.6113 * Math.exp(5423.0 * (1/273.15 - 1/(273.15+x[i-1])));
+
+                    let r_s = 0.622 * e_s / (pls[i-1]/10 - e_s);
+                    let dtdp = ((1.4-1.0)/1.4*(x[i-1]+273.15)+2488.4*r_s)/(pls[i-1]*(1+1.35e7*r_s/Math.pow(x[i-1]+273.15, 2)));
+                    
+                    x[i] = x[i-1] + dtdp*(pls[i]-pls[i-1]);
+                }
+
+                x = x.map((xi: number, i: number) => {
+                    return xi + this.skew * Math.log(pls[0] / pls[i]) 
+                });
+
+                traces.push({x: x, y: pls, mode: "lines", line: { color: "orange", width: 0.8, dash: "dot" }, showlegend: false});
+            });
+
+
+            // isohumes
+            [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0].forEach( (r_s) => {
+                let x: number[] = pls.map((pl: number) => {
+                    return Math.pow(1/273.15-0.0001844*Math.log(r_s/1000.0*pl/10.0/(0.6113*(r_s/1000.0+0.622))), -1) - 273.15 + this.skew * Math.log(pls[0] / pl);
+                });
+                
+                traces.push({x: x, y: pls, mode: "lines", line: { color: "blue", width: 0.8, dash: "dot" }, showlegend: false});
+            });
+
+            Plotly.newPlot("skew-chart", traces, this.layout, {displayModeBar: false});
         }
     },
     components: { },
-    computed: {
-    },
     created: function() {
     },
     mounted: function() {
